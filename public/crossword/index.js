407b9b2a16c6bee846a17bda078f01db9c4a66af
andOrlando <bennettgillig@gmail.com>
2024-02-13 23:49:05 -0500

    spelling bee is good

--- public/crossword/index.js
+++ public/crossword/index.js
@@ -5,8 +5,7 @@ import { getCrossword } from "../api.js"
 //variables
 const all = await getCrossword().then(a => a.json())
 const data = all.body[0]
-const cells = data.cells
-console.log(data)
+console.log(all)
 
 const countdown_elem = document.getElementById("countdown")
 const board_elem = document.getElementById("board")
@@ -17,6 +16,7 @@ const clue_elem = document.getElementById("clue")
 
 //init
 const [WIDTH, HEIGHT] = [data.dimensions.width, data.dimensions.height]
+const SIZES = data.SVG.children[1].children
 const guesses = new Array(WIDTH*HEIGHT).fill(undefined)
 
 //reverse index for clues
@@ -29,6 +29,13 @@ data.clues.forEach(({ cells, direction }, i) => {
 })
 
 board_elem.innerHTML = data.board
+const applyOverlay=()=>all.assets.forEach(({ uri }) => {
+  board_elem.querySelector("svg").innerHTML +=
+    `<image xlink:href="${uri}" width="100%" height="100%" style="pointer-events: none"></image>`
+})
+
+if ((!data.overlays || !data.overlays.afterSolve) && all.assets) applyOverlay()
+
 const cell_elems = [...document.querySelectorAll(".cells g")]
 
 let down = 0
@@ -40,6 +47,44 @@ let rebussing = false
 let ismobile = false;
 (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) ismobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
 
+
+//cookies
+const strstate = {
+  NONE: 0,
+  CORRECT: 1,
+  INCORRECT: 2
+}
+const cookie = localStorage.getItem("guesses")
+function cookie2Guesses(cookie) {
+  const stuff = cookie.split(" ")
+  if (stuff[0] != all.publicationDate) return localStorage.setItem("guesses", undefined)
+  
+  for (const [i, guess] of stuff.entries()) {
+    if (!i || !guess) continue
+    switch (parseInt(guess.substr(0, 1))) {
+      case strstate.CORRECT:
+        cell_elems[i-1].classList.add("correct")
+        break
+      case strstate.INCORRECT:
+        cell_elems[i-1].classList.add("incorrect")
+        break
+    }
+    putStr(i-1, guess.substr(1), true)
+  }
+}
+if (cookie) cookie2Guesses(cookie)
+
+function guesses2Cookie() {
+  return all.publicationDate + " " + guesses.map((a, i) => {
+    if (!a) return ""
+    if (cell_elems[i].classList.contains("correct")) return strstate.CORRECT + a
+    if (cell_elems[i].classList.contains("incorrect")) return strstate.INCORRECT + a
+    return strstate.NONE + a
+  }).join(" ")
+}
+
+
+
 //UI init
 function makeClueElem({ label, text, cells, direction }) {
   const elem = document.createElement("div")
@@ -47,8 +92,28 @@ function makeClueElem({ label, text, cells, direction }) {
   elem.addEventListener("click", () => updateDS((direction=="Down")|0, cells[0]))
   return elem
 }
-const SIZES = data.SVG.children[1].children
-function putStr(i, str) {
+const gamestate = {
+  UNFINISHED: 0,
+  FINISHED: 1,
+  WON: 2,
+}
+function checkGameState() {
+  let done = true
+  let win = true
+  for (const [{ answer }, i] of data.cells.filter(c => c.answer != undefined).entries()) {
+    if (!guesses[i]) {
+      done = false
+      break
+    }
+
+    if (guesses[i] != answer) win = false
+  }
+  if (done && win) return gamestate.WON
+  else if (done) return gamestate.FINISHED
+  return gamestate.UNFINISHED
+}
+
+function putStr(i, str, forcookies=false) {
   if (i == undefined) return
   if (cell_elems[i].classList.contains("correct")) return
   cell_elems[i].classList.remove("incorrect")
@@ -68,9 +133,24 @@ function putStr(i, str) {
 
   text.setAttribute("font-size", font_size)
   const realwidth = text.getBoundingClientRect().width
-  if (realwidth < width) return
+  if (realwidth >= width) text.setAttribute("font-size", text.getAttribute("font-size") / realwidth * width)
 
-  text.setAttribute("font-size", text.getAttribute("font-size") / realwidth * width)
+  //if this is for cookies we can stop here
+  if (forcookies) return
+
+  //upate cookies
+  localStorage.setItem("guesses", guesses2Cookie())
+  
+  //game end check
+  switch (checkGameState()) {
+    case gamestate.WON:
+      console.log("you won!")
+      break
+    case gamestate.FINISHED:
+      console.log("loser!")
+      break
+  }
+
 }
 
 if (!ismobile) data.clues.forEach(c => {
@@ -101,12 +181,6 @@ updateCountdown()
 //keyboard things
 //TODO: this should probably be in a seperate file or smth
 
-const nextClue=mkMoveClueFn(true)
-const prevClue=mkMoveClueFn(false)
-function mkMoveClueFn(next) { return () => {
-    const new_clue = data.clues[(clues[down][selected] + (next ? 1 : -1) + data.clues.length) % data.clues.length]
-    updateDS((new_clue.direction == "Down")|0, new_clue.cells[0])
-}}
 
 let key_state = 0
 const KEY_STATES = [
@@ -158,8 +232,8 @@ function setKeyState(s) {
   }
 }
 if (ismobile) { document.getElementById("keyboard").classList.remove("hide"); setKeyState(0) }
-document.getElementById("nextclue").addEventListener("click", nextClue)
-document.getElementById("prevclue").addEventListener("click", prevClue)
+document.getElementById("nextclue").addEventListener("click", ()=>nextClueBetter())
+document.getElementById("prevclue").addEventListener("click", ()=>prevClueBetter())
 
 const mkKeyDivFn=prop=>(i,j)=>{
   key_elems[i][j].children[0].classList[prop]("hover")
@@ -171,7 +245,7 @@ const keyDivDown=mkKeyDivFn("add")
 console.log(key_elems)
 
 key_elems.forEach((row, i) => row.forEach((button, j) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("touchstart", () => {
 
     //switch key
     if (i == 2 && j == 0) { setKeyState((key_state + 1) % 3) }
@@ -181,7 +255,7 @@ key_elems.forEach((row, i) => row.forEach((button, j) => {
 
       if (rebussing && guesses[selected]) return putStr(selected, guesses[selected].substring(0, guesses[selected].length-1))
 
-      if (!guesses[selected]) moveBackward()
+      if (!guesses[selected]) moveBackward() //moveBackward does literally just do moveBackward
       putStr(selected, undefined)
 
     //otherwise
@@ -190,7 +264,7 @@ key_elems.forEach((row, i) => row.forEach((button, j) => {
       if (rebussing) return putStr(selected, (guesses[selected]||"")+KEY_STATES[key_state][i][j])
 
       putStr(selected, KEY_STATES[key_state][i][j])
-      moveForward()
+      moveForwardBetter()
       
     }
   })
@@ -223,30 +297,32 @@ const highlight_full=mkHighlight("add")
 const unhighlight_full=mkHighlight("remove")
 
 rebus_elem.addEventListener("keydown", e => {
-  if (e.key == "Enter") { unrebus() }
+  if (e.key == "Enter") { rebus(false) }
   if (e.key == "Tab") e.preventDefault()
 })
 rebus_elem.addEventListener("input", ()=>putStr(selected, rebus_elem.value))
-const mkRebus=rebus=>()=>{
-  rebussing = rebus
+/**
+ * sets whether it's rebussing or not to value of on
+ * @param {boolean} on - true means rebussing, false means not
+ */
+function rebus(on) {
+  rebussing = on
 
   //for mobile we don't actually need an input box
-  if (ismobile && rebus) return
+  if (ismobile && on) return
   else if (ismobile) return moveForward()
 
-  rebus_elem.classList[rebus ? "add" : "remove"]("hide")
-  rebus_elem.value = rebus ? guesses[selected] || "" : ""
-  if (rebus) rebus_elem.focus()
-  else moveForward()
+  rebus_elem.classList[on ? "add" : "remove"]("hide")
+  rebus_elem.value = on ? guesses[selected] || "" : ""
+  if (on) rebus_elem.focus()
+  else if (guesses[selected]) moveForward()
 }
-const dorebus=mkRebus(true)
-const unrebus=mkRebus(false)
 
 function updateDS(_down, _selected) {
 
 
   //if literally anything changes, unrebus
-  if (rebussing) unrebus()
+  if (rebussing) rebus(false)
 
   // if selected doesn't exist just highlight simple
   if (selected == undefined) {
@@ -300,7 +376,7 @@ function updateDS(_down, _selected) {
 cell_elems.forEach((a, i) => a.addEventListener("click", () => {
 
   // if we click on black do nothing
-  if (cells[i].type == undefined) { null }
+  if (data.cells[i].type == undefined) { null }
 
   // if we're currently selected, flip
   else if (selected == i) { updateDS(down ^ 1, selected) }
@@ -319,15 +395,16 @@ if (!ismobile) document.addEventListener("keydown", e => {
   if (selected == undefined) return
   
   //if its a character put the character and move forward
-  if (e.key.match(/^[a-zA-Z]$/) != null) {
+
+  if (e.key.match(/^[a-zA-Z0-9\-\/:;()$^@\\.,?!'[\]{}#%^*+=_|~<>€£¥•"]$/)) {
     putStr(selected, e.key)
-    moveForward()
+    moveForwardClipped()
   }
 
   //backspace
   if (e.code == "Backspace") {
     
-    if (!guesses[selected]) moveBackward()
+    if (!guesses[selected]) moveBackwardClipped()
     putStr(selected, undefined)
   }
 
@@ -343,8 +420,8 @@ if (!ismobile) document.addEventListener("keydown", e => {
     if (e.code == "ArrowLeft" || e.code == "ArrowUp") moveBackward()
   }
 
-  if (e.code == "Tab" && e.shiftKey) prevClue()
-  else if (e.code == "Tab") nextClue()
+  if (e.code == "Tab" && e.shiftKey) prevClueBetter()
+  else if (e.code == "Tab") nextClueBetter()
 
   if (["Backspace", "ArrowRight", "ArrowLeft", "ArrowUp", "ArrowDown", "Tab"].includes(e.code))
     e.preventDefault()
@@ -366,7 +443,7 @@ document.getElementById("checkword").addEventListener("click", ()=>word(down, se
 document.getElementById("checkpuzzle").addEventListener("click", ()=>{for (let i=0; i<WIDTH*HEIGHT; i++) checkCell(i)})
 document.getElementById("revealword").addEventListener("click", ()=>word(down, selected).forEach(revealCell))
 document.getElementById("revealpuzzle").addEventListener("click", ()=>{for (let i=0; i<WIDTH*HEIGHT; i++) revealCell(i)})
-document.getElementById("rebusbtn").addEventListener("click", () => rebussing ? unrebus() : dorebus())
+document.getElementById("rebusbtn").addEventListener("click", () => rebus(!rebussing))
 
 
 //helper methods
@@ -375,22 +452,88 @@ function word(down, selected) {
   return data.clues[clues[down][selected]].cells
 }
 
-const moveForward=mkMoveFn((s, w) => s != w[w.length-1], l=>l[0], 1)
-const moveBackward=mkMoveFn((s, w) => s != w[0], l=>l[l.length-1], -1)
-function mkMoveFn(comparator, nextindex, dir) { return () => {
+//used for backspace on mobile
+const moveForward=()=>updateDS(...moveNOUPDATE(down, selected, true, false))
+const moveBackward=()=>updateDS(...moveNOUPDATE(down, selected, false, false))
+//used for letters and backspace on web
+const moveForwardClipped=()=>updateDS(...moveNOUPDATE(down, selected, true, true))
+const moveBackwardClipped=()=>updateDS(...moveNOUPDATE(down, selected, false, true))
+
+/**
+ * Given a down and selected, return the down and selected of the next or previous tile
+ * @param {boolean} next - true means forwards, false means backwards
+ * @param {boolean} clip - whether or not to advance to the next clue when at an extreme
+ */
+function moveNOUPDATE(down, selected, next, clip) {
+  const dir = next ? 1 : -1
   if (selected == undefined) return
   //get current word
   const w = word(down, selected)
 
   //if it's not the last one, just go to the next index
-  if (comparator(selected, w)) {
-    updateDS(down, selected + (down ? WIDTH : 1) * dir )
-    return
+  if (next ? selected != w[w.length-1] : selected != w[0]) {
+    return [down, selected + (down ? WIDTH : 1) * dir]
   }
+
+  // if we're clipping it to the word, don't advance
+  if (clip) return [down, selected]
     
   //if it is the last one we gotta increment the current clue
   const new_clue = data.clues[(clues[down][selected] + dir + data.clues.length) % data.clues.length]
-  updateDS((new_clue.direction == "Down")|0, nextindex(new_clue.cells))
-}}
+  return [(new_clue.direction == "Down")|0, next ? new_clue.cells[0] : new_clue.cells[new_clue.cells.length-1]]
+}
+
+
+/**
+ * Creates a fucntion that will, given a selected and down, move to the
+ * next available open space.
+ */
+function moveBetterNOUPDATE(down, selected, next) {
+  const gs = checkGameState();
+  let [d,s] = moveNOUPDATE(down, selected, next)
+  const w = word(down, selected)
+
+  // if we're filled or if the next is empty, move normally
+  if (gs == gamestate.FINISHED || gs == gamestate.WON || (!guesses[s] && w.includes(s)))
+    return [d,s]
+
+  //set selected to start before trying to moveNOUPDATE
+  selected = w[0];
+  
+  while (guesses[selected]) [down, selected] = moveNOUPDATE(down, selected, next)
+  return [down, selected]
+}
+//used for letters on mobile
+const moveForwardBetter=()=>updateDS(...moveBetterNOUPDATE(down, selected, true))
+// const moveBackwardBetter=()=>updateDS(...moveBetterNOUPDATE(down, selected, false))
+
+/**
+ * Given a down and selected, return the down and selected of the next or previous clue
+ * @param {boolean} next - true means next clue, false means previous clue
+ */
+function clueNOUPDATE(down, selected, next) {
+    const new_clue = data.clues[(clues[down][selected] + (next ? 1 : -1) + data.clues.length) % data.clues.length]
+    return [(new_clue.direction == "Down")|0, new_clue.cells[0]]
+}
+
+/**
+ * Creates function that will go to the next/prev unfilled clue and then the first unfilled cell
+ * @param {boolean} next - true will go to next unfilled clue, false will go to prev unfilled clue
+ */
+function clueBetterNOUPDATE(down, selected, next) {
+  const gs = checkGameState();
+  [down, selected] = clueNOUPDATE(down, selected, next)
+  if (gs == gamestate.FINISHED || gs == gamestate.WON) return updateDS(down, selected)
+
+  while (word(down, selected).every(a => guesses[a])) [down, selected] = clueNOUPDATE(down, selected, next)
+
+  //afterwards if the first char is selected, movebetter
+  if (guesses[selected]) [down, selected] = moveBetterNOUPDATE(down, selected, true)
+  return [down, selected]
+}
+//used for mobile
+const nextClueBetter=()=>updateDS(...clueBetterNOUPDATE(down, selected, true))
+const prevClueBetter=()=>updateDS(...clueBetterNOUPDATE(down, selected, false))
+
 
 
--- public/spellingbee/index.js
+++ public/spellingbee/index.js
@@ -2,17 +2,25 @@ import { getSpellingBee } from "../api.js"
 console.log("doing stuff")
 
 //consts
+//thanks https://mattomath.wordpress.com/2020/11/16/secrets-of-the-spelling-bee/
 const TITLES = ["Beginner", "Good Start", "Moving Up", "Good", "Solid", "Nice", "Great", "Amazing", "Genius", "Queen Bee"]
 const PERCENTAGES = [0, 2, 5, 8, 15, 25, 40, 50, 70, 100]
+const COLORS = ["#99c3f2", "#7db1e8", "#5ea0e6", "#3382d6", "#1965b5", "#16579c", "#194675", "#152e52", "#142040", "#c492fc"]
 
 //variables
 const data = await getSpellingBee().then(a => a.json())
+console.log(data)
 const button_elems = [...document.querySelectorAll(".sbb:not(#center)")]
 const center_elem = document.getElementById("center")
 const word_elem = document.getElementById("word")
 const score_elem = document.getElementById("score")
 const found_elem = document.getElementById("found")
 const rank_elem = document.getElementById("rank")
+const countdown_elem = document.getElementById("countdown")
+const totalprogress_elem = document.getElementById("totalprogress")
+const stageprogress_elem = document.getElementById("stageprogress")
+const scorebox_elem = document.getElementById("scorebox")
+const mostrecent_elem = document.getElementById("mostrecent")
 
 const letters = [...data.outerLetters]
 let score = 0
@@ -23,25 +31,68 @@ const total = data.answers.reduce((sum, a) => sum += scoreWord(a), 0)
 const breakpoints = PERCENTAGES.map(a => Math.round(a * 0.01 * total))
 let rank = 0
 
+//set values with cookies
+async function cookie2Found() {
+  const stuff = (localStorage.getItem("spellingbee") || "").split(",")
+  if (stuff[0] != data.printDate) return localStorage.removeItem("spellingbee")
+  stuff.shift()
+
+  await Promise.resolve(r => setTimeout(r, 1000))
+  for (const word of stuff) {
+    await Promise.resolve(r => setTimeout(r, 300))
+    givePoints(scoreWord(word))
+    findWord(word)
+  }
+}
+function found2Cookie() {
+  localStorage.setItem("spellingbee", data.printDate + "," + found.join(","))
+}
+cookie2Found()
+
 //initialize UI
+const capitalize=w=>w.charAt(0).toUpperCase()+w.slice(1).toLowerCase()
 function setLetters(buttons) {
   for (let i=0; i<buttons.length; i++)
-    buttons[i].innerHTML = letters[i]
+    buttons[i].innerHTML = letters[i].toUpperCase()
 }
 
+const setProgress=(elem, percent)=>elem.setAttribute("style", `--done: ${percent}; --color: ${COLORS[rank]}`)
+setProgress(totalprogress_elem, 0)
+setProgress(stageprogress_elem, 0)
 function updateRank() { rank_elem.innerText = TITLES[rank] }
 function updateScore() { score_elem.innerText = score }
+function scoreboxToast(pts) {
+  const elem = document.createElement("span")
+  elem.innerText = `+${pts}!` 
+  elem.style.left = `${score_elem.offsetWidth + 4 + Math.random() * 4}px`
+  elem.style.bottom = `${Math.random() * 8 - 6}px`
+  elem.classList.add("points")
+  scorebox_elem.appendChild(elem)
+  requestAnimationFrame(() => {
+    elem.classList.add("die")
+  })
+  setTimeout(() => elem.classList.add("diedie"), 900)
+  setTimeout(() => elem.remove(), 1000)
+}
 function givePoints(pts) {
   score += pts
   updateScore()
+  scoreboxToast(pts)
 
-  if (score < breakpoints[rank]) return
+  if (score < breakpoints[rank]) {
+    const last = rank ? breakpoints[rank-1] : 0
+    setProgress(stageprogress_elem, (score-last)/(breakpoints[rank]-last))
+    return
+  }
   while (score >= breakpoints[rank]) rank++
+  setProgress(stageprogress_elem, (score-breakpoints[rank-1])/(breakpoints[rank]-breakpoints[rank-1]))
+  setProgress(totalprogress_elem, rank / (TITLES.length-1))
+  stageprogress_elem.style.background = 
   updateRank()
 }
 
 function makeFoundElem(word) {
-  const elem = document.createElement("li")
+  const elem = document.createElement("div")
   elem.innerText = word
   return elem
 }
@@ -49,14 +100,17 @@ function initFound() {
   for (const word of found)
     found_elem.appendChild(makeFoundElem(word))
 }
-function findWord(word) {
+function findWord(word, cookie=true) {
   found.push(word)
-  found_elem.appendChild(makeFoundElem(word))
+  found_elem.appendChild(makeFoundElem(capitalize(word)))
+  mostrecent_elem.innerText = capitalize(word)
+
+  if (cookie) found2Cookie()
 }
 
 
 setLetters(button_elems)
-center_elem.innerHTML = data.centerLetter
+center_elem.innerHTML = data.centerLetter.toUpperCase()
 initFound()
 updateScore()
 updateRank()
@@ -64,35 +118,59 @@ updateRank()
 //functionality
 
 function addChar(c) {
+
+  // if word is currently empty remove placeholder
+  if (word == "") word_elem.querySelector(".placeholder").remove()
   
   const elem = document.createElement("span")
-  elem.innerText = c
+  elem.innerText = c.toUpperCase()
   elem.classList.add("char")
   if (c == data.centerLetter) elem.classList.add("center")
   
   word_elem.appendChild(elem)
   word += c
 }
+function putPlaceholder() { 
+    const elem = document.createElement("span")
+    elem.innerText = "WORD"
+    elem.classList.add("placeholder")
+    word_elem.appendChild(elem)
+}
+putPlaceholder()
 function removeLastChar() {
   if (word.length == 0) return
+  //otherwise just remove last like normal
   word_elem.removeChild(word_elem.children[word_elem.children.length-1])
   word = word.slice(0, -1)
+
+  if (word == "") putPlaceholder()
 }
 function setWord(s) {
   word_elem.innerHTML = ""
   word = "";
   [...s].forEach(addChar)
 }
+function addTempAttr(elem, name, timeout) {
+  elem.setAttribute(name, (elem.getAttribute(name) || 0) + 1)
+  setTimeout(() => {
+    const count = elem.getAttribute(name) || 0
+    if (count <= 1) elem.removeAttribute(name)
+    else elem.setAttribute(name, count-1)
+  }, timeout)
+}
+
 function submit() {
   if (data.answers.includes(word) && !found.includes(word)) {
     givePoints(scoreWord(word))
     findWord(word)
+    addTempAttr(word_elem, "good", 600)
   }
   else {
-    console.log("word is bad")
+    addTempAttr(word_elem, "bad", 600)
   }
 
   setWord("")
+  putPlaceholder()
 }
 
 center_elem.addEventListener("click", () => addChar(data.centerLetter))
@@ -103,9 +181,13 @@ document.getElementById("shuffle").addEventListener("click", () => {
   shuffle(letters)
   setLetters(button_elems)
 })
-document.getElementById("backspace").addEventListener("click", removeLastChar)
-document.getElementById("clear").addEventListener("click", () => setWord(""))
-document.getElementById("submit").addEventListener("click", submit)
+document.getElementById("delete").addEventListener("click", removeLastChar)
+document.getElementById("enter").addEventListener("click", submit)
+let rotation = 0;
+document.getElementById("dropdown").addEventListener("click", () => {
+    document.getElementById("arrow").style.transform = `rotate(${rotation += 180}deg)`
+    document.getElementById("found").classList.toggle("show")
+})
 
 //universal keypress listener
 document.addEventListener("keydown", e => {
@@ -114,6 +196,17 @@ document.addEventListener("keydown", e => {
   if (e.key == data.centerLetter) addChar(data.centerLetter)
   if (letters.includes(e.key)) addChar(e.key)
 });
+
+let date = new Date(Date.parse(data.printDate))
+date.setHours(date.getHours() - 4) //into EST
+date.setTime(date.getTime() - Date.now() + 5 * 60 * 60 * 1000) //make readable
+
+function updateCountdown() {
+  date.setSeconds(date.getSeconds() - 1)
+  countdown_elem.innerText = date.toTimeString().split(" ")[0]
+  setTimeout(updateCountdown, 1000)
+}
+updateCountdown()
 
 
 
@@ -124,19 +217,6 @@ function scoreWord(word) {
   if (data.pangrams.includes(word)) return word.length + 7
   return word.length
 }
-
-//thanks https://mattomath.wordpress.com/2020/11/16/secrets-of-the-spelling-bee/
-
-
-
-
-function makeCurveFn() {
-  return x => Math.floor(total * Math.sqrt(x / 11))
-}
-
-
-
-
 
 function shuffle(arr) {
   for (let i=0; i<arr.length; i++) {
